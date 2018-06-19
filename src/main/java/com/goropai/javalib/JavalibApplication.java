f9adfb3398ad4195d5bb5afcd13f262670da2d9d
goropai <goropai@ukr.net>
2018-06-19 20:49:42 +0300

    Application started

---
+++ src/main/java/com/goropai/javalib/JavalibApplication.java
@@ -0,0 +1,12 @@
+package com.goropai.javalib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavalibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavalibApplication.class, args);
+	}
+}
---
+++ src/test/java/com/goropai/javalib/JavalibApplicationTests.java
@@ -0,0 +1,16 @@
+package com.goropai.javalib;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JavalibApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
